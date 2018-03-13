6b094f0bc37eb078f0bce7115c96fd8629323eb9
root <root@localhost.localdomain>
2018-03-13 18:49:21 +0800

    init

---
+++ file.lua
@@ -0,0 +1,40 @@
+-- todo: biz begin
+
+-- biz end
+
+
+-- check token
+local token = ngx.req.get_uri_args()["token"]
+local session = ngx.var.cookie_JSESSIONID
+if token and string.len(token)>1 then
+    local sessiondb = ngx.shared.sessiondb
+    local t = sessiondb:get(token)
+    if not t then
+        ngx.say("token id not found")
+        return
+    end
+else
+        ngx.say("session id not found")
+        return
+end
+
+
+--ngx.say(ngx.var.file_name)
+local file_name=ngx.var.file_name
+if file_name and string.len(file_name)>1 then
+	ngx.header.content_type = "application/octet-stream; charset=utf-8"
+
+	ngx.header.content_disposition = "attachment; filename=" .. file_name
+
+	--ngx.header.x_accel_limit_rate = "102400"
+
+	--ngx.header.x_accel_redirect = "/_file/" .. file_name
+	--ngx.header.x_accel_redirect = "/_file/test.txt"
+	ngx.header.x_accel_redirect = file_name
+else
+	ngx.header.content_type = "text/html; charset=utf-8"
+	ngx.say('未指定文件!')
+end
+
+
+
---
+++ token.lua
@@ -0,0 +1,41 @@
+ngx.header.content_type = "text/plain"
+
+
+function getToken()
+--local fff = ngx.re.match('abcd.tar.gz','(\\w+)(.*)')
+--ngx.say(fff[2])
+    math.randomseed(tostring(os.time()):reverse():sub(1, 6))
+    --ngx.say(ngx.md5(math.random()));
+    return ngx.md5(math.random())
+end
+
+
+local sessiondb = ngx.shared.sessiondb
+if not sessiondb then
+    ngx.say("please init sessiondb in http: lua_shared_dict sessiondb XXXm;")
+    return
+end
+
+-- paramers
+--local sid = ngx.var.cookie_JSESSIONID
+local token = getToken()
+local file_type = "jpeg"
+local file_size = "10M"
+-- owner user everyone
+local file_mod = "0777"
+local file_timeout = 3600
+local timestamp = os.time()
+local t = {}
+t = {token=token,file_type=file_type,file_size=file_size,timestamp=timestamp}
+
+local success, err, forcible = sessiondb:set(token,token)
+-- 86400s
+local ok,err = sessiondb:safe_set(token,token,86400)
+if ok then
+    ngx.say('{"sdfs_token":"'..token..'", "code":200, "message":"success!"}')
+else
+    ngx.say(err)
+end
+
+
+
