7655006a408150da8a0309ed0286ca56962ff921
root <root@localhost.localdomain>
2018-03-27 21:39:49 +0800

    添加fastdfs token

--- token.lua
+++ token.lua
@@ -9,6 +9,13 @@ function getToken()
     return ngx.md5(math.random())
 end
 
+-- FastDFS1234567890 http.conf
+function getFDFSToken(remote_filename,ts,secret_key)
+	--local ts = os.time()
+	--local secret_key = "FastDFS1234567890"
+	return ngx.md5(remote_filename .. secret_key .. ts)
+end
+
 
 local sessiondb = ngx.shared.sessiondb
 if not sessiondb then
@@ -16,6 +23,7 @@ if not sessiondb then
     return
 end
 
+local fdfs_file = ''
 local sid = ''
 local token = getToken()
 local file_type = "jpeg"
@@ -27,6 +35,9 @@ local file_timeout = 86400
 local timestamp = os.time()
 
 -- paramers
+if ngx.req.get_uri_args()["fdfs_file"] then
+    fdfs_file = ngx.req.get_uri_args()["fdfs_file"]
+end
 if ngx.req.get_uri_args()["sid"] then
     sid = ngx.req.get_uri_args()["sid"]
 end
@@ -37,11 +48,16 @@ end
 local t = {}
 t = {sid=sid,file_type=file_type,file_size=file_size,file_timeout=file_timeout,timestamp=timestamp}
 
+local fdfs_token = ''
+if fdfs_file~='' then
+    fdfs_token = getFDFSToken(fdfs_file,timestamp,"FastDFS1234567890")
+end
+
 local cjson = require 'cjson';
 --local success, err, forcible = sessiondb:set(token,cjson.encode(t))
 local ok,err = sessiondb:safe_set(token,cjson.encode(t),file_timeout)
 if ok then
-    ngx.say('{"token":"'..token..'", "code":200, "message":"success!", "data":'..cjson.encode(t)..'}')
+    ngx.say('{"token":"'..token..'", "fdfs_token":"'..fdfs_token..'", "code":200, "message":"success!", "data":'..cjson.encode(t)..'}')
 else
     ngx.say(err)
 end
